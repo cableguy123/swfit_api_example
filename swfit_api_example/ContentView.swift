be8dcf5812155ae9bb5998a956f082d74213b2cf
imseoghyeon <cableguy123@naver.com>
2025-05-17 17:24:08 +0900

    Initial Commit

---
+++ swfit_api_example/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  swfit_api_example
+//
+//  Created by 임석현 on 2025/05/17.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ swfit_api_example/swfit_api_exampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  swfit_api_exampleApp.swift
+//  swfit_api_example
+//
+//  Created by 임석현 on 2025/05/17.
+//
+
+import SwiftUI
+
+@main
+struct swfit_api_exampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
